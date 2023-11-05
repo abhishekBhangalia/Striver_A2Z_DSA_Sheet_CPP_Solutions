7a8fcee7adf249edc21804088ad93de0e5f973c6
Abhishek Bhangalia <abhangalia0001@gmail.com>
2023-11-05 12:04:33 +0530

    Update 4. Frog Jump with k distances.cpp

--- 16. Dynamic Programming/4. Frog Jump with k distances.cpp
+++ 16. Dynamic Programming/4. Frog Jump with k distances.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 
 // dp using memoization
-
+// TC-O(n*k), SC-O(n) + O(n)(auxialiary stack space)
 // int dp(int n, int k, vector<int>& height, vector<int>& cost){
 //     if(n==1){
 //         return 0;
@@ -30,22 +30,40 @@
 // }
 
 // dp using tabulation method
+// TC-O(n*k), SC-O(n)
+// int minimizeCost(int n, int k, vector<int> &height){
+//     // Write your code here.
+//     vector<int> cost(n+1,0);
+//     cost[2] = abs(height[1]-height[0]);
 
-int minimizeCost(int n, int k, vector<int> &height){
+//     for(int stone = 3; stone<=n; stone++){
+//         int min_cost = INT_MAX;
+//         for(int i=1; i<=k; i++){
+//             if(stone-i < 1) break;
+//             int cur_cost = cost[stone-i]+abs(height[stone-1]-height[stone-1-i]);
+//             min_cost = min(min_cost,cur_cost);
+//         }
+//         cost[stone] = min_cost;
+//     }
+//     return cost[n];
+// }
+
+
+// dp using tabulation and further space optimization
+// TC-O(n*k), SC-O(k)
+int minimizeCost(int n, int k, vector<int> &h){
     // Write your code here.
-    vector<int> cost(n+1,0);
-    cost[2] = abs(height[1]-height[0]);
+    int* cost = new int[k];
+    cost[0] = 0;
 
-    for(int stone = 3; stone<=n; stone++){
+    for(int stone = 1; stone<n; stone++){
         int min_cost = INT_MAX;
-        for(int i=1; i<=k; i++){
-            if(stone-i < 1) break;
-            int cur_cost = cost[stone-i]+abs(height[stone-1]-height[stone-1-i]);
+        for(int step=1; step<=k && stone-step>=0; step++){
+            int cur_cost = cost[(stone-step)%k] + abs(h[stone]-h[stone-step]);
             min_cost = min(min_cost,cur_cost);
         }
-        cost[stone] = min_cost;
+        cost[stone%k] = min_cost;
     }
-    return cost[n];
+    return cost[(n-1)%k];
 }
-
 
