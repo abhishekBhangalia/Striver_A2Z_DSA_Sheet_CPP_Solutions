6f6f6f477b990e508ca8bf2eb81de235f27a3aed
Abhishek Bhangalia <abhangalia0001@gmail.com>
2024-03-19 18:21:43 +0530

    Create 30. Dijkstras Algo.cpp

---
+++ Graphs/30. Dijkstras Algo.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    //TC - O(E log(h)), h height of min heap
+    vector<int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        // Code here
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        vector<int> dis(V,INT_MAX);
+        dis[S] = 0;
+        pq.push({0,S});
+        
+        while(!pq.empty()){
+            pair<int,int> v = pq.top();
+            pq.pop();
+            for(vector<int> u: adj[v.second]){
+                if(dis[v.second]+u[1] < dis[u[0]]){
+                    dis[u[0]] = dis[v.second] + u[1];
+                    pq.push({dis[u[0]],u[0]});
+                }
+            }
+        }
+        
+        return dis;
+        
+    }
+};
