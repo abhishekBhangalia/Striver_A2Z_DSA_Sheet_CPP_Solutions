3640fdcb4d3358ed10529765a665e78398e6fbad
Abhishek Bhangalia <abhangalia0001@gmail.com>
2024-04-14 09:52:38 +0530

    Update 30. Dijkstras Algo.cpp

--- Graphs/30. Dijkstras Algo.cpp
+++ Graphs/30. Dijkstras Algo.cpp
@@ -4,26 +4,81 @@ class Solution
 	//Function to find the shortest distance of all the vertices
     //from the source vertex S.
     //TC - O(E log(v)), h height of min heap
-    vector<int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    vector<int> dijkstra(int n, vector<vector<int>> edges, int S)
     {
         // Code here
+	    S = 0;
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        vector<int> dis(V,INT_MAX);
-        dis[S] = 0;
-        pq.push({0,S});
+        vector<vector<pair<int,int>>> graph(n);
+        for(auto &e : edges){
+            graph[e[0]].push_back({e[1],e[2]});  
+            graph[e[1]].push_back({e[0],e[2]});  
+        }
+        
+        vector<int> dis(n,INT_MAX);
+        dis[0] = 0;
+        pq.push({0,0});
         
         while(!pq.empty()){
             pair<int,int> v = pq.top();
+
             pq.pop();
-            for(vector<int> u: adj[v.second]){
-                if(dis[v.second]+u[1] < dis[u[0]]){
-                    dis[u[0]] = dis[v.second] + u[1];
-                    pq.push({dis[u[0]],u[0]});
+            
+            if(v.first != dis[v.second])
+                continue;
+            for(auto &u: graph[v.second]){
+                
+                int newDis = dis[v.second]+u.second;
+                if(newDis < dis[u.first] and newDis < disappear[u.first]){
+                    dis[u.first] = newDis;
+                    pq.push({dis[u.first],u.first});
                 }
             }
         }
         
+        for(auto &x : dis){
+            if(x == INT_MAX){
+                x = -1;
+            }
+        }
         return dis;
+        
+        // set<pair<int,int>> st; 
+
+        // vector<int> dist(n, INT_MAX); 
+        
+        // st.insert({0, 0}); 
+
+        // dist[0] = 0;
+        
+        // while(!st.empty()) {
+        //     auto it = *(st.begin()); 
+        //     int node = it.second; 
+        //     int dis = it.first; 
+        //     st.erase(it); 
+            
+        //     for(auto &it : graph[node]) {
+        //         int adjNode = it.first; 
+        //         int edgW = it.second; 
+        //         int newDis = dis + edgW;
+                
+        //         if(newDis < dist[adjNode] and newDis < disappear[adjNode]) {
+
+        //             if(dist[adjNode] != INT_MAX) 
+        //                 st.erase({dist[adjNode], adjNode}); 
+
+        //             dist[adjNode] = newDis; 
+        //             st.insert({dist[adjNode], adjNode}); 
+        //          }
+        //     }
+        // }
+       
+        // for(auto &x : dist){
+        //     if(x == INT_MAX){
+        //         x = -1;
+        //     }
+        // }
+        // return dist; 
         
     }
 };
